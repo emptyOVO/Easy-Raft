543be1c0673d90705adae1c536a1a429ad20e15c
emptyOVO <gaoyifei040229@163.com>
2025-02-08 15:58:00 +0800

    format check

--- easy-raft-example/src/main/java/com/emptyovo/raft/example/AdminMain.java
+++ easy-raft-example/src/main/java/com/emptyovo/raft/example/AdminMain.java
@@ -1,7 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.emptyovo.raft.example;
 
 import com.emptyovo.raft.proto.RaftProto;
-import com.emptyovo.raft.service.RaftClientService;
 import com.googlecode.protobuf.format.JsonFormat;
 import org.apache.commons.lang3.Validate;
 
@@ -12,6 +28,7 @@ import java.util.List;
  * Example
  */
 public class AdminMain {
+
     private static final JsonFormat jsonFormat = new JsonFormat();
 
     public static void main(String[] args) {
